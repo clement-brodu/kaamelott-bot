3eb1255a8aa183abc92ddcb40e828ea3a9573d64
Clément Brodu <clement.brodu@gmail.com>
2018-11-07 00:40:26 +0100

    Fix : Bug sur le fallback

--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -4,7 +4,7 @@
 module.exports = function (controller) {
 
     controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
-		var mardown = "La vache, je comprend pas un mot de ce que vous racontez.<br/"
+		var mardown = "La vache, je comprend pas un mot de ce que vous racontez.<br/>"
             + "Pour avoir de l'aide, il n'y a qu'à demander : " + bot.appendMention(rawMessage, "help")
             
         bot.reply(message, mardown);
